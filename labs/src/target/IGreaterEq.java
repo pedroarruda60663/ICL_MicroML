041346b60921698f7b07100bf701d713b0330289
Gonçalo Gingeira <gugagu2002@gmail.com>
2024-04-11 14:21:56 +0100

    instructions

---
+++ labs/src/target/IGreaterEq.java
@@ -0,0 +1,9 @@
+package target;
+
+public class IGreaterEq extends Instruction{
+
+        public IGreaterEq() {
+            op = "igreatereq";
+            args = null;
+        }
+}
---
+++ labs/src/target/ILessEq.java
@@ -0,0 +1,9 @@
+package target;
+
+public class ILessEq extends Instruction {
+
+    public ILessEq() {
+        op = "ilesseq";
+        args = null;
+    }
+}
